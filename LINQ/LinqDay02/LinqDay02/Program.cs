1ed0c3a0053de111fd75ec86bb7517c955f27658
Ammar Hammad <amargamal832909@gmail.com>
2024-01-21 14:08:01 +0200

    LinqDay02 Added

--- LINQ/LinqDay02/LinqDay02/Program.cs
+++ LINQ/LinqDay02/LinqDay02/Program.cs
@@ -1,4 +1,5 @@
-﻿using static LinqDay02.ListGenerators;
+﻿using System.Text.RegularExpressions;
+using static LinqDay02.ListGenerators;
 
 
 namespace LinqDay02
@@ -229,6 +230,199 @@ namespace LinqDay02
              Console.WriteLine(ProductList.Sum(p => p.UnitsInStock));*/
 
             #endregion
+
+            #region Generators Operators 
+
+            /* var result = Enumerable.Range(0, 20);
+
+             var r01 = Enumerable.Empty<Product>();
+
+             var r02 = Enumerable.Repeat(3 , 10);
+
+             var r03 = Enumerable.Repeat(ProductList[0] , 5);
+ */
+
+
+            #endregion
+
+
+
+            List<string> nameLists = new List<string>()
+            {
+                "Ahmed Ali" , "Sally Samir" , "Ammar Gamal"
+            };
+
+            #region Select Many
+            // output seq count > input seq count
+            // transform each element in input seq to subseq (IEnumerable<>)
+
+            /*var result = nameLists.SelectMany(FN => FN.Split(' '));
+
+            // Query Syntax : use Multiple from to produce Select Many 
+
+            result = from FN in nameLists
+                     from SN in FN.Split(" ")
+                     orderby SN.Length descending
+                     select SN;
+
+            var r01 = nameLists.SelectMany(SN => SN.ToCharArray());*/
+
+
+            #endregion
+
+
+            #region Casting Operators : Imediate Excution 
+            /*
+                        List<Product> unAvPrds = ProductList.Where(p => p.UnitsInStock == 0).ToList();
+
+                        Product[] pArr = ProductList.Where(p => p.UnitsInStock != 0)
+                                                    .OrderBy(p => p.UnitPrice).ToArray();
+
+                        var result = ProductList.Where(p => p.UnitsInStock == 0)
+                                                //.ToHashSet();
+                                                //.ToDictionary(p => p.ProductID) // Dic <long , Product>
+                                                //.ToDictionary(p => p.ProductID, p => new { p.ProductID, p.ProductName });
+                                                // Dic <long , Anonymous [long,string]>
+                                                //.ToLookup();
+            */
+            #endregion
+
+
+
+            #region Set Operators 
+            /*var seq01 = Enumerable.Range(0, 100); // 0 : 99
+            var seq02 = Enumerable.Range(50, 100); // 50 : 99 ,, start from 50 and sum += 100
+
+            var result = seq01.Union(seq02); // remove duplicates 
+
+            result = seq01.Concat(seq02); // save duplicates 
+            result = result.Distinct(); // remove duplicates
+
+            result = seq01.Except(seq02);
+            result = seq01.Intersect(seq02);
+
+            foreach (var item in result)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();*/
+
+            #region Index Range C# 8.0
+            /*
+            var lst1 = lst01.ToList();
+
+            Console.WriteLine(lst1[0]);
+            Console.WriteLine(lst1[^1]);
+            Console.WriteLine(lst1[^5]);
+
+
+            Range result = 1..^10;*/
+
+            #endregion
+
+
+
+            #endregion
+
+            #region Quantifiers , return boolean
+
+            /*Console.WriteLine(ProductList.Any()); // return true if input seq has atleast 1 element
+*/
+            // Console.WriteLine(ProductList.Any(p => p.UnitPrice > 200));
+            // return true if input seq have atleast 1 element matches the condition
+
+            // Console.WriteLine(ProductList.All(p => p.UnitsInStock > 0));
+            // return true of all elements in input seq matches the conditions 
+
+            /*var seq01 = Enumerable.Range(0, 100);
+            var seq02 = Enumerable.Range(50, 100);
+
+            Console.WriteLine( seq01.SequenceEqual(seq02));
+*/
+
+
+
+
+            #endregion
+
+
+            #region Zip
+            // input 2 seq , one combined o/p seq
+            /*var lst02 = Enumerable.Range(0, 10);
+            
+            var result = nameLists.Zip(lst02 , (FN , i) => new {i , Name = FN.ToUpper()});*/
+
+
+            #endregion
+
+            #region Grouping
+
+            /*var result = from p in ProductList
+                         where p.UnitsInStock > 0
+                         group p by p.Category
+                         into prdGroups
+                         where prdGroups.Count() >= 10
+                         orderby prdGroups.Count() descending
+                         select new { Category = prdGroups.Key, prdCount = prdGroups.Count() };
+
+            result = ProductList.GroupBy(p => p.Category).Where(prdG => prdG.Count() >= 10)
+                .OrderByDescending(pg => pg.Count())
+                .Select(pg => new { Category = pg.Key, prdCount = pg.Count() });*/
+
+            /*foreach ( var prdGroup in result ) {
+                Console.WriteLine($"Group Key {prdGroup.Key}");
+
+                *//*foreach(var prd in prdGroup )
+                {
+                    Console.WriteLine($".. {prd.ProductName}");
+                }*//*
+            }*/
+
+            #endregion
+
+            #region Let / Into - Introducing new Range Variable in Query Syntax
+
+            List<string> Names = new List<string>()
+            {
+                "Ahmed" , "Aly" , "Samir" , "Mai" , "Sally" , "Moemen" , "Shimaa" , "Mohammed"
+            };
+
+            // Regular Expression
+            /*var result = from N in Names
+                         select Regex.Replace(N, "[aeiouAEIOU]", string.Empty)
+                        // Restart Query with new range variable , Old Range Variable is not Accessable
+                        into NoVol
+                         where NoVol.Length >= 3
+                         orderby NoVol, NoVol.Length descending
+                         select NoVol;
+*/
+           var result = from N in Names
+                     let NoVol = Regex.Replace(N , "[aeiouAEIOU]" , string.Empty)
+                     where NoVol.Length >= 3
+                     orderby NoVol , N.Length descending
+                     select NoVol;
+
+            #endregion
+
+
+
+            
+             foreach(var item in result)
+             {
+                 Console.WriteLine(item);
+             }
+            /*
+            foreach(var item in r01)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            foreach (var item in r03)
+            {
+                Console.WriteLine(item + " ");
+            }*/
+
+
 
 
             Console.ReadKey();
